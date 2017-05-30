8c59f24c52fd37bda2bb4bc1bb6e8c289fac27ef
sheng <sheng@intern01.(none)>
2017-05-30 13:52:17 -0400

    added argument to specify words

--- randpass.py
+++ randpass.py
@@ -5,16 +5,16 @@
 # A Silly random password generator 
 
 import linecache
+import sys
 from random import randint
 
-rnum1 = randint(1,1000)
-word1 = linecache.getline('common-words.txt', rnum1)
-rnum2 = randint(1,1000)
-word2 = linecache.getline('common-words.txt', rnum2)
-rnum3 = randint(1,1000)
-word3 = linecache.getline('common-words.txt', rnum3)
+nwords = sys.argv[1]
+words = []
 
-line = word1.rstrip()+word2.rstrip().title()+word3.rstrip().title()+str(randint(1000,9999)).rstrip()
+for i in xrange(int(nwords, 10)):
+	words.append(linecache.getline('common-words.txt', randint(1,1000)).rstrip().title())
+
+line = "".join(words)+str(randint(1000,9999)).rstrip()
 
 print "Your random password is:", line
 
