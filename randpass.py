c1be195ea8d640080cc32642ce9a679fdfe1581b
failedrequest <msaad@lucera.com>
2018-12-13 12:01:50 -0500

    Renamed to randpass

--- randpass.py
+++ randpass.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python2.7
 # Mark Saad 
 # mark.saad@ymail.com
-# 7July2016
+# 12Dec2018
 # A Silly random password generator 
 
 import linecache
 from random import randint
 
-rnum1 = randint(1,1000)
-word1 = linecache.getline('common-words.txt', rnum1)
-rnum2 = randint(1,1000)
-word2 = linecache.getline('common-words.txt', rnum2)
-rnum3 = randint(1,1000)
-word3 = linecache.getline('common-words.txt', rnum3)
 
-line = word1.rstrip()+word2.rstrip().title()+word3.rstrip().title()+str(randint(1000,9999)).rstrip()
+def getword():
+    word = linecache.getline('common-words.txt', randint(1,1000))
+    return word.strip()
 
-print "Your random password is:", line
+
+print("Your random password is: {}{}{}{}").format(getword(),getword().title(),getword().title(),randint(1000,9999))
 
